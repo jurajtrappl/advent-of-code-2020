5ecfa77d6a62cc83328e45852d2dfa8278bf36df
Juraj Trappl <juro.trappl@gmail.com>
2021-11-26 19:30:13 +0100

    add the solution to 10. problem (first part)

---
+++ 10/10.hs
@@ -0,0 +1,17 @@
+module T10 where
+    import Data.List
+
+    parseInput :: IO [Int]
+    parseInput = do
+        input <- readFile "10.in"
+        return $ map (\s -> read s :: Int) $ lines input
+
+    pairsDiff :: [Int] -> [Int]
+    pairsDiff [x] = []
+    pairsDiff (x:y:ys) = y - x : pairsDiff (y:ys)
+
+    fstPart :: IO ()
+    fstPart = do
+        input <- parseInput
+        let diffs = pairsDiff $ sort ([0] ++ input ++ [maximum input + 3])
+        print $ length (filter (==1) diffs) * length (filter (==3) diffs)
